383111927e202aaa90ad924fe57e16300d228fae
Rohit Jain <46364854+rohit1804567@users.noreply.github.com>
2022-06-26 23:41:52 +0530

    Update UnionFIndCompression.cpp
    added _union

--- UnionFIndCompression.cpp
+++ UnionFIndCompression.cpp
@@ -1,10 +1,26 @@
 int head(unordered_map<int,int>m, int vertex)
-  {
-      while(m[vertex] != vertex)
-      {
-          // we optimise it for that particular node, that nodes goes to its top parent
-          m[vertex] = m[m[vertex]];
-          vertex = m[vertex];
-      }
-      return vertex;
-  }
+{
+    while(m[vertex] != vertex)
+    {
+        // we optimise it for that particular node, that nodes goes to its top parent
+        m[vertex] = m[m[vertex]];
+        vertex = m[vertex];
+    }
+    return vertex;
+}
+
+
+
+void _union(int a,int b, unordered_map<int,int>&size,unordered_map<int,int>&m)
+{
+    if (size[a] < size[b])
+    {
+        size[b] += size[a];
+        m[b] = m[a];
+    }
+    else
+    {
+        size[a] += size[b];
+        m[a] = m[b];
+    }
+}
